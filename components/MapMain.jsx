bff3b8697e0b3d6c42db1cb9b3f17f5bcd4cc5e4
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-10-09 08:39:31 -0600

    development commit

--- components/MapMain.jsx
+++ components/MapMain.jsx
@@ -7,10 +7,10 @@ const MapMain = () => {
             <h1 className="heading text-black">
                 EONET<span className="text-purple"></span>
             </h1>
-            <p className="text-center md:tracking-wider mb-4 m4 text-sm md:text-lg lg:text-2xl text-black">
+            <p className="text-black md:mt-10 my-5 text-center">
                 The Earth Observatory Natural Event Tracker
                 More and more NASA imagery is being made available via web services and a significant percentage of it is being produced and published in near real time. <br />
-                This ability means that NASA imagery can be used more routinely to examine current natural events as they happen.Using client applications. Users can browse the entire globe daily and look for natural events as they occur. <br />
+                This ability means that NASA imagery can be used more routinely to examine current natural events as they happen.Using client applications. Users can browse the entire globe daily and look for natural events as they occur.
                 Storms are regularly spotted in the tropics, dust storms over deserts, forest fires in the summers. These events are occurring constantly and NASA NRT imagery can represent them all using a variety of different data parameters.
             </p>
             <a href="https://map-eonet-nasa.vercel.app/" >
